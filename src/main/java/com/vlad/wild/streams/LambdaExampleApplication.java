c794da07de7a4388bb06cc6db75bf09b4a0bede7
Gusev Vladislav <v.v.gusev@tinkoff.ru>
2022-05-01 15:44:04 +0300

    add stream example

--- src/main/java/com/vlad/wild/streams/LambdaExampleApplication.java
+++ src/main/java/com/vlad/wild/streams/LambdaExampleApplication.java
@@ -1,6 +1,10 @@
 package com.vlad.wild.streams;
 
+/**
+ * Что выведется в 1-ом и во 2-ом случаях?
+ */
 public class LambdaExampleApplication {
+    private int k = 10;
 
     public static void main(String[] args) {
         LambdaExampleApplication application = new LambdaExampleApplication();
@@ -8,7 +12,17 @@ public class LambdaExampleApplication {
     }
 
     public void startApplication() {
+        Runnable lambda = () -> System.out.println(this.k);
+        lambda.run();
 
+        Runnable anonymousClass = new Runnable() {
+            private int k = 9999;
 
+            @Override
+            public void run() {
+                System.out.println(this.k);
+            }
+        };
+        anonymousClass.run();
     }
 }
